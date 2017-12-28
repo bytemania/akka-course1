048cd7e295103f337628709473b9675323c7409c
António <Silva>
2017-12-28 22:26:47 +0000

    gitignore

---
+++ src/main/scala/cluster/Cluster.scala
@@ -0,0 +1,43 @@
+package cluster
+
+import akka.actor.{Actor, ActorSystem, Props, RootActorPath}
+import akka.cluster.Cluster
+import akka.cluster.ClusterEvent.MemberUp
+import com.typesafe.config.ConfigFactory
+
+case class Add(num: Int, num2: Int)
+
+case object BackendRegistration
+
+class Backend extends Actor {
+  val cluster = Cluster(context.system)
+
+  override def preStart(): Unit = {
+    cluster.subscribe(self, classOf[MemberUp])
+  }
+
+  override def postStop(): Unit = {
+    cluster.unsubscribe(self)
+  }
+
+  override def receive: Receive = {
+    case Add(num1, num2) =>
+      println(s"I'm a backend with path: ${self} and I received add operation")
+    case MemberUp(member) =>
+      if (member.hasRole("frontend")) {
+        context.actorSelection(RootActorPath(member.address) / "user" / "frontend") ! BackendRegistration
+      }
+  }
+}
+
+object Backend {
+  def initiate(port: Int) = {
+    val config = ConfigFactory.parseString(s"akka.remote.netty.tcp.port=$port")
+      .withFallback(ConfigFactory.load().getConfig("Backend"))
+
+    val system = ActorSystem("ClusterSystem", config)
+
+    val Backend = system.actorOf(Props[Backend], name = "Backend")
+  }
+}
+
